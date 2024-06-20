f8eff58229a548e6b4a50600dbfd7ae944796cb7
MeghaB94 <meghabhagat94@gmail.com>
2024-06-19 20:49:25 -0700

    use translated csv

--- local_dump/dump_data_to_sql.py
+++ local_dump/dump_data_to_sql.py
@@ -1,5 +1,5 @@
 import os
-import pandas as pd
+from pandas import DataFrame, read_csv, concat
 from .local_translator import translate_columns, translate_rows
 from data_cleaner import clean_data
 from db import mysql_engine
@@ -15,7 +15,7 @@ ANSI_ENCODING = os.getenv("ANSI_ENCODING")
 
 
 def read_files_to_separate_dataframes(directory, keywords):
-    excel_data = {keyword: pd.DataFrame() for keyword in keywords}
+    excel_data = {keyword: DataFrame() for keyword in keywords}
     # Walk through the directory
 
     for root, dirs, files in os.walk(directory):
@@ -24,7 +24,7 @@ def read_files_to_separate_dataframes(directory, keywords):
                 if keyword in file.lower() and file.lower().endswith(".csv"):
                     file_path = os.path.join(root, file)
                     try:
-                        csv_data = pd.read_csv(
+                        csv_data = read_csv(
                             file_path,
                             dtype=str,
                             encoding=(
@@ -35,7 +35,7 @@ def read_files_to_separate_dataframes(directory, keywords):
                         csv_data = csv_data.apply(
                             lambda x: x.str.strip() if x.dtype == "object" else x
                         )
-                        excel_data[keyword] = pd.concat(
+                        excel_data[keyword] = concat(
                             [excel_data[keyword], csv_data], ignore_index=True
                         )
                     except Exception as e:
@@ -43,11 +43,23 @@ def read_files_to_separate_dataframes(directory, keywords):
     return excel_data
 
 
+def _clean_and_dump_to_db(dataframes: dict):
+    for keyword, df in dataframes.items():
+        if type(df) == DataFrame:
+            print(f"cleaning data for {keyword}")
+            dataframes[keyword] = df = clean_data(df, keyword)
+            print(f"data cleaned and ready to write to database for {keyword}")
+            result = df.to_sql(
+                name=keyword, con=mysql_engine, index=False, if_exists="replace"
+            )
+            print(result)
+
+
 def save_to_sql():
     dataframes = read_files_to_separate_dataframes(directory, keywords)
 
     for keyword, df in dataframes.items():
-        if type(df) == pd.DataFrame:
+        if type(df) == DataFrame:
             print(f"cleaning data for {keyword}")
             df = df[
                 ~df["Username"].str.contains(
@@ -67,9 +79,10 @@ def save_to_sql():
             df = translate_columns(df)
             df = translate_rows(df)
             df.to_csv(f"{translated_directory}/{keyword}.csv", index=False)
-            dataframes[keyword] = df = clean_data(df, keyword)
-            print(f"data cleaned and ready to write to database for {keyword}")
-            result = df.to_sql(
-                name=keyword, con=mysql_engine, index=False, if_exists="replace"
-            )
-            print(result)
+            dataframes[keyword] = df
+    _clean_and_dump_to_db(dataframes)
+
+
+def dump_translated_to_sql():
+    dataframes = read_files_to_separate_dataframes(translated_directory, keywords)
+    _clean_and_dump_to_db(dataframes)
